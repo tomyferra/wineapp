7adaf4a3c86d234460620df357a97dd8a5c1a715
tomyferra <ferra.tomy@gmail.com>
2023-03-10 13:31:53 -0300

    center text

--- src/components/WineList.js
+++ src/components/WineList.js
@@ -8,7 +8,7 @@ function WineList() {
     <div className="winelist container-fluid">
       <div className="row">
         <div className="container">
-          <div className="row wines-value">
+          <div className="row justify-content-center wines-value">
             {Wines.map( (wine) => (
                 <div className="col col-sm-4 wines-style">
                   <Wine name={wine.Name} winery={wine.Winery} description={wine.Description} year={wine.Year} variety={wine.Variety} totalratings={wine.Totalqualifications} avgratings={wine.Avgqualifications} region={wine.Region} imgpath={wine.Image} totalscore={wine.Totalscore}/>
